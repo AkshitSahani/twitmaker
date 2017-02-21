33a0377b3fefb72a15059ef648dd5973ab513469
Akshit Sahani <akshit_sahani@hotmail.com>
2017-02-21 16:24:16 -0500

    made an HTML response work

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function(){
+
+  $('#new_tweet').on('submit', function(event){
+    event.preventDefault();
+
+  $.ajax({
+    // url: $(this).attr('action'),
+    url: $(this).attr('action'),
+    method: $(this).attr('method'),
+    data: $(this).serialize()
+  }).done(function(data){
+    console.log(data);
+    $('.tweets').prepend(data);
+  });
+ })
+})
